fa0d86ddbc1ad87e7d275e0341f9f452f64dd243
Vladislav <vladislavrnd@gmail.com>
2020-10-27 23:15:16 +0300

    minor changes again

--- src/components/appHeader.js
+++ src/components/appHeader.js
@@ -12,10 +12,10 @@ class AppHeader extends Component {
         return (
             <header className="app_header d-flex justify-content-between align-items-center">
                 <a className="mr-auto p-2" href="/" >
-                <img draggable="false" src="/images/cards.svg" alt="english cards" />
+                    <img draggable="false" src="/images/cards.svg" alt="english cards" />
                 </a> 
                 <a className="p-2" href="/game" >Start</a>
-                <a className="p-2" href="/cards/" >Cards</a>
+                <a className="p-2" href="/cards" >Cards</a>
                 <a className="p-2" rel="noopener noreferrer" target="_blank" href="https://github.com/Batarbin/english-cards" >GitHub</a>
             </header>
         )

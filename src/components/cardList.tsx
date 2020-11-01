bde8bf33065d5d4f74368500c1561bfe0e39ee70
Vladislav <vladislavrnd@gmail.com>
2020-11-01 23:14:41 +0300

    added scroll to top button to card list page

--- src/components/cardList.tsx
+++ src/components/cardList.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Card, CardImg } from 'reactstrap'
 import { GetCardList } from '../actions/cardListActions'
@@ -28,15 +28,31 @@ function CardList() {
     const dispatch = useDispatch()
     const cardListState = useSelector((state: RootStore) => state.cardListState)
     const { cardList, cardListLoaded, cardListLoading } = cardListState
+    const [showScroll, setShowScroll] = useState(false)
 
     useEffect(() => {
         dispatch(GetCardList())
     }, [dispatch])
+    useEffect(() => {
+        window.addEventListener('scroll', checkScrollTop)
+        function checkScrollTop() {
+            if (!showScroll && window.pageYOffset > 400){
+                setShowScroll(true)
+            } else if (showScroll && window.pageYOffset <= 400){
+                setShowScroll(false)
+            }
+        }
+        return () => {
+            window.removeEventListener('scroll', checkScrollTop)
+        }
+    })
+    function scrollTop() {
+        window.scrollTo({top: 0, behavior: 'smooth'})
+    }
 
     if (!cardListLoaded) {
         return <ServerError />
     }
-    
     if (!cardList || !cardList.length || cardListLoading) {
         return (
             <LoadingSpinner />
@@ -44,16 +60,19 @@ function CardList() {
     }
     
     return (
-        <div className="card_list_wrapper d-flex flex-column align-items-center justify-content-center">
-            <div className="card_list"> 
-                <h3 className="text-center">List of cards:</h3>
-                <Row className="d-flex align-items-center justify-content-center"> 
-                    {cardList.map(item => <CardItem
-                        key={item.id}
-                        {...item}
-                    />)}
-                </Row>
-            </div>
+        <div className="card_list d-flex flex-column align-items-center justify-content-center">
+            <img draggable="false" src="/images/up-arrow.png" alt="scroll to top"
+                className="scrollTop" 
+                onClick={scrollTop} 
+                style={{display: showScroll ? 'flex' : 'none'}}
+            />
+            <h3 className="text-center">List of cards:</h3>
+            <Row className="d-flex align-items-center justify-content-center"> 
+                {cardList.map(item => <CardItem
+                    key={item.id}
+                    {...item}
+                />)}
+            </Row>
         </div>
     )
 }
